5065fdeeb04595c002856a3e22f36649fa2cac06
aristoneangelo <aristoneangelo@gmail.com>
2025-07-13 01:38:18 +0100

    feat: configs material ui cssbaseline, Themeprovider and themecreate

---
+++ src/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme, responsiveFontSizes } from "@mui/material";
+
+
+let theme = createTheme({
+    palette: {
+        primary: {
+        main: "#d4cfcfff",
+        },
+        secondary: {
+        main: "#046523ff",
+        },
+    },
+    typography:{
+        fontFamily: "Segoe UI, Roboto, Helvetica, Arial, sans-serif",
+    }
+});
+theme = responsiveFontSizes(theme);
+export default theme;
