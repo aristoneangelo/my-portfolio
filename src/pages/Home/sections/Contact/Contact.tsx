23cab28d790d528c3c62b2a4b1a4f8206d12bcf9
aristoneangelo <aristoneangelo@gmail.com>
2025-07-20 12:14:58 +0100

    feat: create components section contact

---
+++ src/pages/Home/sections/Contact/Contact.tsx
@@ -0,0 +1,28 @@
+
+import { Grid, styled } from "@mui/material"
+import * as SkillIcons from '../../../../assets/images/SkillsIcons';
+
+const Contact = () => {
+    const StyledContact = styled("div")(({theme}) => ({
+        background: theme.palette.primary.main,
+    }))
+    return (
+        <StyledContact>
+            <Grid container spacing={2}>
+                <Grid size={12} padding={1} display={"flex"} justifyContent={"center"} alignContent={"center"}>
+                    <a href="http://">
+                        <img src={SkillIcons.github} width={"40%"} height={"40%"} />
+                    </a>
+                    <a href="http://">
+                        <img src={SkillIcons.github} width={"40%"} height={"40%"} />
+                    </a>
+                    <a href="http://">
+                        <img src={SkillIcons.github} width={"40%"} height={"40%"} />
+                    </a>
+                </Grid>
+            </Grid>
+        </StyledContact>
+    )
+}
+
+export default Contact
