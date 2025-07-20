80f90f7a617dcbf0548d2337a27bdf7b5c3af186
aristoneangelo <aristoneangelo@gmail.com>
2025-07-20 12:30:43 +0100

    style: add effect motion.div in home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,35 @@
 import NavBar from "../../components/NavBar/NavBar";
+import Contact from "./sections/Contact/Contact";
 import Hero from "./sections/Hero/Hero";
+import Projects from "./sections/Projects/Projects";
 import Skills from "./sections/Skills/Skills";
+import { motion } from "motion/react"
 
 const Home = () => {
   return (
     <>
       <NavBar />
-      <Hero />
-      <Skills />
-
+      <motion.div
+        initial={{opacity:0, scale:0}}
+        whileInView={{opacity:1, scale:1}}
+        transition={{duration: 2}}>
+        <Hero />
+      </motion.div>
+      <motion.div 
+        initial={{opacity:0, translateX:"100%"}}
+        whileInView={{opacity:1, translateX:0}}
+        transition={{duration: 1}}>
+        <Skills />
+      </motion.div>
+      <motion.div
+        initial={{opacity:0, rotateX:"90deg"}}
+        whileInView={{opacity:1, rotateX:0}}
+        transition={{duration: 2}}>
+            <Projects/>
+       </motion.div>
+        <motion.div>
+            <Contact/>
+        </motion.div>
     </>
   );
 }
