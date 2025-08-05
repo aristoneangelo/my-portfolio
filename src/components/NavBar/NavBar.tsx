c707e6d3678591e2ff7f7e4a6049da568e7772dd
aristoneangelo <aristoneangelo@gmail.com>
2025-08-05 09:02:34 +0100

    feat: add home item in appbar menu

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -10,6 +10,7 @@ const NavBar = () => {
     <>
       <AppBar position="relative" color="primary">
         <StyledToolbar>
+          <MenuItem>Home</MenuItem>
           <MenuItem>About</MenuItem>
           <MenuItem>Skills</MenuItem>
           <MenuItem>Projects</MenuItem>
